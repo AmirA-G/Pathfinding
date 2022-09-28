6d577c4d611e328a1160397cee0040eeaf4e5521
Twilight <48221256+AmirA-G@users.noreply.github.com>
2022-09-28 09:20:38 +0100

    up

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public bool isDebug = true;
+    public Transform[] waypoints;
+    public float Length
+    {
+        get{
+            return waypoints.Length;
+        }
+    }
+
+    public Vector3 GetPoint(int index)
+    {
+        return waypoints[index].position;
+    }
+
+    /// <summary>
+    /// Show Debug Grids and obstacles inside the editor
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        if (!isDebug)
+            return;
+
+        for (int i = 1; i < waypoints.Length; i++){
+            Debug.DrawLine(waypoints[i - 1].position, waypoints[i].position, Color.red);
+        }
+    }
+}
